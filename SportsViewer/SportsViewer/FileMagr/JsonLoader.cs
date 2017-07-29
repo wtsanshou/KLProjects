a6080a0c3074e9524976af61d90d8005b40dea2c
wtsanshou <158300054@qq.com>
2017-07-29 17:41:55 +0100

    Add read file demo

---
+++ SportsViewer/SportsViewer/FileMagr/JsonLoader.cs
@@ -0,0 +1,17 @@
+﻿using SportsViewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsViewer.FileMagr
+{
+    class JsonLoader
+    {
+        public RugbyTeam loadFile(string filePath)
+        {
+        return null;
+        }
+    }
+}
--- SportsViewer/SportsViewer/Program.cs
+++ SportsViewer/SportsViewer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SportsViewer.FileMagr;
+using SportsViewer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,25 @@ namespace SportsViewer
     {
         static void Main(string[] args)
         {
+            JsonLoader fileLoader = new JsonLoader();
+            string filePath = "Files/rugby_athletes.json";
+
+            while (true)
+            {
+                Console.WriteLine("Enter File Path:");
+                filePath = Console.ReadLine();
+                RugbyTeam rubyTeams = fileLoader.loadFile(@filePath);
+                for (int i = 0; i < rubyTeams.athletes.Count; ++i)
+                {
+                    Console.WriteLine(rubyTeams.athletes[i].name);
+                }
+
+                for (int j = 0; j < rubyTeams.squads.Count; ++j)
+                {
+                    Console.WriteLine(rubyTeams.squads[j].name);
+                }
+            }
         }
+
     }
 }
