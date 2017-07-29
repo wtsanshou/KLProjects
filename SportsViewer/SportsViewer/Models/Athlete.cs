c8c84dbcd85af6e1fdcd8f972eadea5cee1791ac
wtsanshou <158300054@qq.com>
2017-07-29 17:31:16 +0100

    Add models

---
+++ SportsViewer/SportsViewer/Models/Athlete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsViewer.Models
+{
+    class Athlete
+    {
+        public string avatar_url { get; set; }
+
+        public string country { get; set; }
+
+        public string last_played { get; set; }
+
+        public string name { get; set; }
+
+        public string position { get; set; }
+
+        public bool is_injured { get; set; }
+
+        public int id { get; set; }
+    }
+}
---
+++ SportsViewer/SportsViewer/Models/RugbyTeam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsViewer.Models
+{
+    class RugbyTeam
+    {
+        public List<Athlete> athletes { get; set; }
+
+        public List<Squad> squads { get; set; }
+    }
+}
